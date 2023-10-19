328712b875e7f4417f2a97db3ebd7ab112ec38ae
Mihail <99266606+mik4rok@users.noreply.github.com>
2023-10-19 12:04:40 +0300

    Update rotate_crop.sh

--- rotate_crop.sh
+++ rotate_crop.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
-#ffmpeg -i img1.jpg -vf crop=3390:3390:1056:275 img20.jpg
-HPICT=400              # height of picture
-WPICT=400              # width of picture
-WCROP=96               # width of cropping image
-HCROP=96               # heihht of cropping image
-XCONER=$WPICT/2-$WCROP/2  # left top point x coordinate
-IMAX=16
-JMAX=16
+
+HPICT=400                   # height of picture
+WPICT=400                   # width of picture
+WCROP=96                    # width of cropping image
+HCROP=96                    # heihht of cropping image
+XCONER=$WPICT/2-$WCROP/2    # left top point x coordinate
+IMAX=45                     # max angle
+JMAX=16                     # count of up down horizont line -2
+OUTDIR="./out"
 i=0
 j=0
 
+if [ ! -d $OUTDIR ]
+    then
+        mkdir $OUTDIR
+    else
+        echo "$OUTDIR already exists" 1>&2
+fi
+
+
 for image in ./in/*.jpg
 do
-ffmpeg -i $image -vf scale=$WPICT:$HPICT,setsar=1:1 $image -y
+ffmpeg -i $image -vf scale=$WPICT:$HPICT,setsar=1:1 $image -y   # resize inner images
 name=${image##*/}
 base=${name%.jpg}
     for (( j=2; j < $JMAX-1; j++ ))
     do
-        for (( i=0; i < $IMAX; i++ ))
+        for (( i=0; i < $IMAX; i=i+3 ))     # 3 degreeses step
             do
                 YCONER=$HPICT/2-$HCROP+$j*$HCROP/$JMAX  # left top point x coordinate
                 ANGLE=($i-$IMAX/2)*PI/120
-                ffmpeg -i $image -vf "rotate=$ANGLE, crop=$WCROP:$HCROP:$XCONER:$YCONER" ./out/"$i"."$j"."$name" #
+                ffmpeg -i $image -vf "rotate=$ANGLE, crop=$WCROP:$HCROP:$XCONER:$YCONER" "$OUTDIR"/"$i"."$j"."$name" #
             done
     done
 done
